e20f6474067e5cac2eddbc8bff2ba3ffe411383c
김지원 <kimjw6@brandi.co.kr>
2021-07-25 12:01:44 +0900

    init

---
+++ deepdive/index.js
@@ -0,0 +1,17 @@
+    const $counter = document.getElementById('counter-x');
+    const $increase = document.getElementById('increase');
+    const $decrease = document.getElementById('decrease');
+
+    let num = 0;
+    const render = function () { $counter.innerHTML = num; };
+
+    $increase.onclick = function () {
+      num++;
+      console.log('increase 버튼 클릭', num);
+      render();
+    };
+    $decrease.onclick = function () {
+      num--;
+      console.log('decrease 버튼 클릭', num);
+      render();
+    }
